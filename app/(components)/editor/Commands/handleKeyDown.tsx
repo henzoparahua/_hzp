24d5c5f4b6002ca4a6ea8a1792a3626174724f3a
henzo.ayres <hnz.2894henzop19@gmail.com>
2025-03-07 12:02:22 -0300

    + Handling logic for this sheesh

--- app/(components)/editor/Commands/handleKeyDown.tsx
+++ app/(components)/editor/Commands/handleKeyDown.tsx
@@ -4,19 +4,21 @@ import { Editor, Element, Range, Transforms } from "slate";
 
 export const onKeyDown = (event: React.KeyboardEvent, editor: Editor) => {
   const { selection } = editor;
+  const [match] = Editor.nodes(editor, {
+    match: (n: any) => n.type === "terminal",
+  });
   if (selection && Range.isCollapsed(selection)) {
     const [start] = Range.edges(selection);
     const wordBefore = Editor.before(editor, start, { unit: "word" });
     const beforeRange = wordBefore && Editor.range(editor, wordBefore, start);
     const beforeText = beforeRange && Editor.string(editor, beforeRange);
 
+    console.log(beforeText);
+
     if (beforeText?.startsWith("/")) {
       console.log(`Dentro de /: ${beforeText}`);
-      const [match] = Editor.nodes(editor, {
-        match: (n: any) => n.type === "terminal",
-      });
+
       if (!match) {
-        console.log(`não é um terminal mas agora vai virar`);
         Transforms.setNodes(
           editor,
           { type: "terminal", children: [{ text: "" }] },
@@ -24,8 +26,17 @@ export const onKeyDown = (event: React.KeyboardEvent, editor: Editor) => {
         );
       }
     }
+    if (event.key === "Enter" && match && beforeText?.startsWith("code")) {
+      event.preventDefault();
+      console.log("hehe");
+      Transforms.insertNodes(
+        editor,
+        { type: "code", children: [{ text: "" }] },
+        { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
+      );
+    }
   }
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && !match) {
     event.preventDefault();
     Transforms.insertNodes(
       editor,
